bf1b3615e05bb281d7abc7857b46ff8c19b26e26
Ant1955 <abb1955@gmail.com>
2024-08-16 17:08:54 +0300

    for lesson 2

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('new', views.new, name='page2')
+    path('new', views.new, name='page2'),
+    path('other', views.other, name='page3'),
+    path('last', views.last, name='page4')
 ]
 
--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html',{'caption':"CatDjango"})
+    return render(request, 'main/index.html',{'caption':"DzenCat"})
 
 
 def new(request):
     data = {
-        'caption':"CatDjango"
+        'caption':"Cat Number 2"
     }
     return render(request, 'main/new.html',data)
+def other(request):
+    return render(request, 'main/other.html',{'caption':"3th Cat"})
+def last(request):
+    return render(request, 'main/last.html',{'caption':"Cat last"})
